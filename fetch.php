4801de8219abd99658d572273dd5966df42b0462
Ken Chapple <ken@mi-squared.com>
2021-12-16 09:04:37 -0500

    remove junk with locking

--- fetch.php
+++ fetch.php
@@ -21,29 +21,6 @@ set_time_limit(0);
 
 require_once(__DIR__."/../../../globals.php");
 
-// Make sure there is not another fetch.php process running
-$lock_file_directory = $GLOBALS['OE_SITE_DIR'] . DIRECTORY_SEPARATOR . 'documents';
-$lock_file = fopen($lock_file_directory . 'fetch.pid', 'c');
-$got_lock = flock($lock_file, LOCK_EX | LOCK_NB, $wouldblock);
-if ($lock_file === false || (!$got_lock && !$wouldblock)) {
-    $message = "Unexpected error opening or locking lock file. Perhaps you " .
-        "don't  have permission to write to the lock file or its " .
-        "containing directory?";
-    //throw new Exception($message);
-    error_log($message);
-    exit;
-}
-else if (!$got_lock && $wouldblock) {
-    $message = "Another instance is already running; terminating.\n";
-    error_log($message);
-    exit($message);
-}
-
-// Lock acquired; let's write our PID to the lock file for the convenience
-// of humans who may wish to terminate the script.
-ftruncate($lock_file, 0);
-fwrite($lock_file, getmypid() . "\n");
-
 $server_id = $argv[1];
 if ($server_id === null) {
     echo "Server ID was not set\n";
@@ -62,8 +39,3 @@ if ($server->isFetchEnabled()) {
     $batch = new \Mi2\SFTP\Models\FetchFileBatch($server);
     $batch->fetch();
 }
-
-// All done; we blank the PID file and explicitly release the lock
-// (although this should be unnecessary) before terminating.
-ftruncate($lock_file, 0);
-flock($lock_file, LOCK_UN);
